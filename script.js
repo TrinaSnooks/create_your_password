10ec32678ba545543915e4e0885ff08eece38c8f
Trina Snooks <trina.snooks@gmail.com>
2022-11-09 23:30:51 -0400

    draft 1.

---
+++ script.js
@@ -0,0 +1,87 @@
+//criteria - 8 -128 characters -- number  ---DONE
+// prompt for number. alert if under 8, above 128  ---DONE
+//upper, lower, number, spec   -- boolean  --DONE
+// confirm - ok/cancel  -- DONE
+//return password in box
+//use a LOOP  - add a random character out of chosen criteria for final password
+// loop will run the number of times the user selected for length -- 
+// strings act like an array??
+// use Math.Random ()  - randomize password
+
+function generatePassword() {
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var number = "1234567890";
+    var special = "!@#$%^&*";
+
+    var finalPassword = "";
+
+    var passwordLength = 
+        prompt ("How many characters should your password be?", "Select 8 to 128 characters");
+
+         console.log(passwordLength);
+
+          if (passwordLength <8 || passwordLength > 128) {
+          alert("Invalid password length. Please select again");
+           
+          return;           
+
+          }
+
+         console.log(passwordLength);
+
+         //ask user for Character types.  All boolean questions
+
+         // declaration of character variables 
+         var isUppercase = confirm("Uppercase?"); 
+         var isLowercase = confirm("Lowercase?");
+         var isNumber = confirm("Numbers?");
+         var isSpecial = confirm("Special Characters?");
+
+         //print results to console from character questions to user
+         console.log(isUppercase);
+         console.log(isLowercase);
+         console.log(isNumber);
+         console.log(isSpecial);
+
+         //creating passwordq
+         if (isUppercase) {
+          finalPassword += uppercase;
+          
+         }
+
+         if(isLowercase) {
+          finalPassword += lowercase;
+         }
+
+         if (isNumber) {
+          finalPassword += number;
+         }
+
+         if(isSpecial) {
+          finalPassword += special;
+         }
+
+//loop
+
+
+}
+
+generatePassword ();
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
