4172d0af1ecfbd5ba7623b337d4ca80ab2b2153a
Trina Snooks <trina.snooks@gmail.com>
2022-11-11 22:27:06 -0400

    making corrections to prompts, alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,52 +8,50 @@
 // strings act like an array??
 // use Math.Random ()  - randomize password
 
-/*
+
 function generatePassword() {
-    /* var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var number = "1234567890";
     var special = "!@#$%^&*";
 
     var finalPassword = "";
-    */
-
+  
+  
     var passwordLength = 
         prompt("How many characters should your password be?", "Select 8 to 128 characters");
           if (passwordLength <8 || passwordLength > 128) 
           alert("Invalid password length. Please select again");         
           
 
-    // show password length in console
-    console.log(passwordLength);
-//}
-
-
-/*
     //ask user for Character types.  All boolean questions
     // declaration of character variables 
-    var isUppercase = confirm("Uppercase?"); 
-    var isLowercase = confirm("Lowercase?");
-    var isNumber = confirm("Numbers?");
-    var isSpecial = confirm("Special Characters?");
 
-    //print results to console from character questions to user
+    var isUppercase = 
+        confirm("Uppercase?");
+
+    var isLowercase = 
+        confirm("Lowercase?");
+
+    var isNumber = 
+        confirm("Numbers?");
+
+    var isSpecial = 
+        confirm("Special Characters?");
+
+    console.log(passwordLength);
     console.log(isUppercase);
     console.log(isLowercase);
     console.log(isNumber);
     console.log(isSpecial);
-
+}
     //creating password
     // add 1 character to finalPassword with +=
-
+/*
          if (isUppercase) {
           finalPassword += uppercase;
-         
-            //userSelection.push(upperCase);
-          //} 
+          } 
           
-         }
-
          if (isLowercase) {
           finalPassword += lowercase;
          }
@@ -65,17 +63,21 @@ function generatePassword() {
          if (isSpecial) {
           finalPassword += special;
          }
+    
+    
+            
+/*
+            for (var i= 0; < passwordLength; i++) {
+              var randomIndex = math.floor(math.random () * userSelection.length);
+              var random = userSelection[randomIndex];
+              finalPassword = finalPassword.concat(random);
+            } */
 
-         
+        //}      
 
 //loop    look at      https://www.programiz.com/javascript/examples/generate-random-strings
 
-*/
-//}
 /*
-generatePassword ();
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
